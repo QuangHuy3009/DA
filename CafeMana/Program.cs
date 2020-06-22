1025afb72a048fd1d0fbeb5bccb3db2cf32786a0
phan tu <pt27122000@gmail.com>
2020-06-22 10:06:00 +0700

    DTO

--- CafeMana/Program.cs
+++ CafeMana/Program.cs
@@ -14,7 +14,7 @@ namespace CafeMana
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormGeneral());
